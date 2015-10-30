6a91863677eb80e8bdbe393a48072f51d58c3e94
Orlandy Ariel <alu0100773408@ull.edu.es>
2015-10-30 11:32:50 +0000

    Añadido los test de las operaciones +,-,* y /

--- test/tc_test.rb
+++ test/tc_test.rb
@@ -7,7 +7,12 @@ class Test_Fraccionario < Test::Unit::TestCase
         @fraccion1 = Fraccionario.new(2, 3)
         @fraccion2 = Fraccionario.new(4, 9)
     end
-  
+    def test_simple
+        assert_operator(@fraccion1 , '+' , @fraccion2, "Suma")
+        assert_operator(@fraccion1 , '-' , @fraccion2, "Resta")
+        assert_operator(@fraccion1 , '/' , @fraccion2, "Division")
+        assert_operator(@fraccion1 , '*' , @fraccion2, "Multiplicacion")
+    end
     def test_type_check
         assert_raise(TypeError){Fraccionario.new(5,0)}
     end
